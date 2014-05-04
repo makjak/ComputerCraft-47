8fe2a09fe429fa03b09ebc910ca67f96866f7dd9
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-05-03 23:42:05 -0300

    feat(bloodmagic_craft): split functionality into 2 computers
       One computer is responsible for showing buttons
       Second is responsible for crafting in altar

--- programs/CraftingInBloodmagicAltar/computer_bloodmagic_craft.lua
+++ programs/CraftingInBloodmagicAltar/computer_bloodmagic_craft.lua
@@ -1,108 +1,72 @@
-os.unloadAPI('buttons')
-os.loadAPI('buttons')
-
-local monSide = 'right'
-local deploySide = 'left'
-local collectSide = 'back'
+local modemSide = 'bottom'
+local putSide = 'right'
+local getSide = 'back'
 
 local blankWait = 6
 local reinforcedWait = 21
 local imbuedWait = 36
 local demonicWait = 54
 
-local monitor = peripheral.wrap(monSide)
-buttons.setDefaultOutput(monitor)
-
 local running = true
-local doingStuff = false
-local waitInterval = 0
+local off = true
+local wait = 0
+local monitorServer
 
-local defaultTextColor = colors.white
-local defaultBgColor = colors.red
-local defaultBgPressedColor = colors.lime
-local allBtns = {}
-
-draw = function()
-   buttons.draw()
-end
-
-enableBtn = function(id)
-   for _, v in pairs(allBtns) do
-      if (v == id) then
-         buttons.setColor(v, defaultTextColor, defaultBgPressedColor)
-      else
-         buttons.setColor(v, defaultTextColor, defaultBgColor)
-      end
-   end
-end
-
-deploy = function(side)
+sendRedstoneSignal = function(side)--{{{
    redstone.setOutput(side, true)
    os.sleep(0.5)
    redstone.setOutput(side, false)
+end--}}}
+
+getMonitorServerId = function() --{{{
+   rednet.broadcast('PING')
+   local eventTable = {os.pullEvent}
+   local event = eventTable[1]
+
+   if (event == 'rednet_message') then
+      local sender, msg, distance = eventTable[2], eventTable[3], eventTable[4]
+
+      if (msg == 'PING') then
+         monitorServer = sender
+      end
+   end
+end--}}}
+
+while (not monitorServer) do
+   getMonitorServerId()
 end
 
-offBtn = function()
-   enableBtn(allBtns.off)
-   draw()
-
-   doingStuff = false
-   waitInterval = 0
-end
-
-blankBtn = function()
-   enableBtn(allBtns.blank)
-   draw()
-
-   doingStuff = true
-   waitInterval = blankWait
-end
-
-reinforcedBtn = function()
-   enableBtn(allBtns.reinforced)
-   draw()
-
-   doingStuff = true
-   waitInterval = reinforcedWait
-end
-
-imbuedBtn = function()
-   enableBtn(allBtns.imbued)
-   draw()
-
-   doingStuff = true
-   waitInterval = imbuedWait
-end
-
-demonicBtn = function()
-   enableBtn(allBtns.demonic)
-   draw()
-
-   doingStuff = true
-   waitInterval = demonicWait
-end
-
-allBtns.off = buttons.register(2, 2, 3, 2, defaultTextColor, defaultBgColor, 'Off', offBtn)
-allBtns.blank = buttons.register(13, 2, 5, 2, defaultTextColor, defaultBgColor, 'Blank', blankBtn)
-allBtns.reinforced = buttons.register(5, 6, 10, 2, defaultTextColor, defaultBgColor, 'Reinforced', reinforcedBtn)
-allBtns.imbued = buttons.register(2, 10, 6, 2, defaultTextColor, defaultBgColor, 'Imbued', imbuedBtn)
-allBtns.demonic = buttons.register(11, 10, 7, 2, defaultTextColor, defaultBgColor, 'Demonic', demonicBtn)
-
-enableBtn(allBtns.off)
-draw()
 while (running) do
-   local eventTable = {os.pullEvent()}
-   buttons.event(eventTable)
+   rednet.send(getMonitorServerId, 'slateState')
 
-   if (doingStuff) then
-      deploy(deploySide)
-      os.sleep(waitInterval)
-      deploy(collectSide)
-      os.sleep(2)
+   local eventTable = {os.pullEvent()}
+   local event = eventTable[1]
+
+   if (event == 'rednet_message') then
+      local sender, msg, distance = eventTable[2], eventTable[3], eventTable[4]
+
+      if (msg == '0') then
+         off = true
+         wait = 0
+      elseif (msg == '1') then
+         off = false
+         wait = blankWait
+      elseif (msg == '2') then
+         off = false
+         wait = reinforcedWait
+      elseif (msg == '3') then
+         off = false
+         wait = imbuedWait
+      elseif (msg == '4') then
+         off = false
+         wait = demonicWait
+      end
    end
 
-   buttons.draw()
+   if (not off) then
+      sendRedstoneSignal(putSide)
+      os.sleep(waitInterval)
+      sendRedstoneSignal(getSide)
+   end
 end
 
-monitor.clear()
-os.unloadAPI('buttons')
---
+++ programs/CraftingInBloodmagicAltar/computer_bloodmagic_monitor.lua
@@ -0,0 +1,98 @@
+os.unloadAPI('buttons')
+os.loadAPI('buttons')
+
+-- Specifies which slate is supposed to be doing
+-- 0 - off
+-- 1 - blank
+-- 2 - reinforced
+-- 3 - imbued
+-- 4 - demonic
+local slateState = 0
+local running = true
+
+local monSide = 'bottom'
+local monitor = peripheral.wrap(monSide)
+buttons.setDefaultOutput(monitor)
+
+local defaultTextColor = colors.white
+local defaultBgColor = colors.red
+local defaultBgPressedColor = colors.lime
+local allBtns = {}
+
+draw = function()
+   buttons.draw()
+end
+
+enableBtn = function(id)
+   for _, v in pairs(allBtns) do
+      if (v == id) then
+         buttons.setColor(v, defaultTextColor, defaultBgPressedColor)
+      else
+         buttons.setColor(v, defaultTextColor, defaultBgColor)
+      end
+   end
+end
+
+offBtn = function()
+   enableBtn(allBtns.off)
+   -- draw()
+
+   slateState = 0
+end
+
+blankBtn = function()
+   enableBtn(allBtns.blank)
+   -- draw()
+
+   slateState = 1
+end
+
+reinforcedBtn = function()
+   enableBtn(allBtns.reinforced)
+   -- draw()
+
+   slateState = 2
+end
+
+imbuedBtn = function()
+   enableBtn(allBtns.imbued)
+   -- draw()
+
+   slateState = 3
+end
+
+demonicBtn = function()
+   enableBtn(allBtns.demonic)
+   -- draw()
+
+   slateState = 4
+end
+
+allBtns.off = buttons.register(2, 2, 3, 2, defaultTextColor, defaultBgColor, 'Off', offBtn)
+allBtns.blank = buttons.register(13, 2, 5, 2, defaultTextColor, defaultBgColor, 'Blank', blankBtn)
+allBtns.reinforced = buttons.register(5, 6, 10, 2, defaultTextColor, defaultBgColor, 'Reinforced', reinforcedBtn)
+allBtns.imbued = buttons.register(2, 10, 6, 2, defaultTextColor, defaultBgColor, 'Imbued', imbuedBtn)
+allBtns.demonic = buttons.register(11, 10, 7, 2, defaultTextColor, defaultBgColor, 'Demonic', demonicBtn)
+
+enableBtn(allBtns.off)
+draw()
+while (running) do
+   local eventTable = {os.pullEvent()}
+   local event = eventTable[1]
+   
+   if (event == 'rednet_message') then
+      local sender, msg, distance = eventTable[2], eventTable[3], eventTable[4]
+
+      if (msg == 'PING') then
+         rednet.send(sender, 'PING')
+      elseif (msg == 'slateState') then
+         rednet.send(sender, slateState)
+      end
+   elseif (event == 'monitor_touch') then
+      buttons.event(eventTable)
+      buttons.draw()
+   end
+end
+
+monitor.clear()
+os.unloadAPI('buttons')
